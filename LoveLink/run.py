90e58b08073cc9a154c1896b23a36ab68602ea61
mrinmay20 <122849711+mrinmay20@users.noreply.github.com>
2026-02-13 13:16:32 +0530

    Update run.py

--- LoveLink/run.py
+++ LoveLink/run.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from app.models import LovePage
-
+import os
 app = create_app()
 
 # 🔥 Create database tables automatically
@@ -8,4 +8,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+    #app.run(debug=True)
+     app.run(host="0.0.0.0", port=port, debug=False)
